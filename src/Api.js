479e34010639d05208901535d76e83858e41e75f
Annie Huang <annie.hn@gmail.com>
2019-12-21 21:49:49 +1100

    export fetchData which allows us to return the special resource that is needed to use Suspense

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,46 @@
 import axios from 'axios';
 
+export const fetchData = () => {
+    const userPromise = fetchUser();
+    const postsPromise = fetchPosts();
+    return {
+        user: wrapPromise(userPromise),
+        posts: wrapPromise(postsPromise)
+    }
+};
+
+const wrapPromise = (promise) => {
+    // Set initial status
+    let status = 'pending';
+    // Store result
+    let result;
+    // Wait for promise
+    let suspender = promise.then(
+        res => {
+            status = 'success';
+            result = res;
+        },
+        err => {
+            status = 'error';
+            result = err;
+        }
+    );
+
+    return {
+        read() {
+            if (status === 'pending') {
+                throw suspender;
+
+            } else if (status === 'error') {
+                throw result;
+
+            } else if (status === 'success') {
+                return result;
+            }
+        }
+    }
+};
+
 const fetchUser = () => {
     console.log('Fetch User...');
     return axios.get('https://jsonplaceholder.typicode.com/users/1')
